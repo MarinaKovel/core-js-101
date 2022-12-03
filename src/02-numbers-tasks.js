cce8ca73360b018a6546e73a552e06bdce774c5c
Marina Kovel <waveee@gmail.com>
2022-12-03 16:01:22 +0300

    feat: resolve number tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -106,8 +106,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const skalMult = x1 * x2 + y1 * y2;
+  const modulX = Math.sqrt(x1 ** 2 + y1 ** 2);
+  const modulY = Math.sqrt(x2 ** 2 + y2 ** 2);
+  return Math.acos(skalMult / (modulX * modulY));
 }
 
 /**
@@ -122,8 +125,8 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return +value.toString().slice(-1);
 }
 
 /**
@@ -137,8 +140,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return +value;
 }
 
 /**
@@ -154,8 +157,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 /**
@@ -175,8 +178,22 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  let result;
+  switch (pow) {
+    case 0:
+      result = Math.round(num);
+      break;
+    case 1:
+      result = 10 * Math.round(num / 10);
+      break;
+    case 2:
+      result = 100 * Math.round(num / 100);
+      break;
+    default:
+      result = 1000 * Math.round(num / 1000);
+  }
+  return result;
 }
 
 /**
@@ -196,8 +213,14 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  let dev = true;
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      dev = false;
+    }
+  }
+  return n > 1 && dev;
 }
 
 /**
@@ -215,8 +238,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return +value || def;
 }
 
 module.exports = {
