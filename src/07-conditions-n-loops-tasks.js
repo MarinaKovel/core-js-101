94b17e85a7041edb96d58fa7b5a087cea972d477
Marina Kovel <waveee@gmail.com>
2022-12-25 18:28:57 +0300

    feat: resolve some tasks

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -132,8 +132,11 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  return (((rect1.left <= rect2.left && rect2.left <= rect1.left + rect1.width) || (rect1.left
+    >= rect2.left && rect2.left + rect2.width >= rect1.left)) && ((rect1.top <= rect2.top
+      && rect2.top <= rect1.top + rect1.height) || (rect1.top >= rect2.top
+        && rect2.top + rect2.height >= rect1.left)));
 }
 
 /**
@@ -162,8 +165,9 @@ function doRectanglesOverlap(/* rect1, rect2 */) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  return Math.sqrt(((point.x - circle.center.x) ** 2) + (point.y - circle.center.y) ** 2)
+  < circle.radius;
 }
 
 /**
